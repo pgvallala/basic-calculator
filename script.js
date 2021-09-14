37ecfce0d3fe15b16974ff85f0160813ed829d02
pranav157 <pranavgupta.v@gmail.com>
2021-09-14 22:21:41 +0200

    evaluate  function

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
 const buttons = document.querySelectorAll(".num-button");
 const allbuttons = document.querySelectorAll(".button");
-const resultBox = document.querySelectorAll("#result-box");
-const clearBtn = document.querySelectorAll("#clear");
-const total = document.querySelectorAll("#total");
+const resultBox = document.querySelector("#result-box");
+const clearBtn = document.querySelector("#clear");
+const total = document.querySelector("#total");
+const resetBtn = document.querySelector("#reset");
 
-let btns = [...buttons];
-let allBtns = [...allbuttons];
+const btns = [...buttons];
+const allBtns = [...allbuttons];
 
 btns.forEach((button, i) => {
   button.addEventListener("click", () => {
-    if ((resultBox.innerHTML = "0")) {
+    if (resultBox.innerHTML == "0") {
       resultBox.innerHTML = "";
     }
     let value = buttons[i].innerHTML;
     resultBox.innerHTML += value;
   });
 });
+
+function evaluate(n) {
+    return new Function('return ' + n)();
+}
+
+total.addEventListener('click', () => {
+    let allInputs = resultBox.innerHTML;
+    resultBox.innerHTML = evaluate(allInputs);
+    console.log(evaluate(allInputs));
+})
+
+clearBtn.addEventListener('click', () => {
+    resultBox.innerHTML = "0";
+})
+
+resetBtn.addEventListener('click', () => {
+    resultBox.innerHTML = "0"
+})
